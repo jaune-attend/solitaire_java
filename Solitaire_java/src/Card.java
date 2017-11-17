4bc11ad99475837bcb5d0acb7aac0c30320a32db
Vincent <vincent.cotini96@gmail.com>
2017-11-17 17:12:44 +0300

    Add files via upload

---
+++ Solitaire_java/src/Card.java
@@ -0,0 +1,98 @@
+
+
+
+public class Card {
+
+	public boolean red;
+	public boolean visible;
+	public int value;
+	public char color;
+		
+	public Card(boolean red, int value, char color) {
+		
+		this.red = red;
+		this.visible = false;
+		this.value = value;
+		this.color = color;
+	}
+	
+	public boolean getRed()
+	{
+		return this.red;
+	}
+	
+	public boolean getVisible() 
+	{	
+		return this.visible;
+	}
+	
+	public int getValue()
+	{
+		return this.value;
+	}
+	
+	public char getColor()
+	{
+		return this.color;
+	}
+	
+	public void setRed(boolean red)
+	{
+		this.red = red;
+	}
+	
+	public void setVisible(boolean visible)
+	{
+		this.visible = visible;
+	}
+	
+	public void setValue(int value)
+	{
+		this.value = value;
+	}
+	
+	public void setColor(char color)
+	{
+		this.color = color;
+	}
+	
+	
+	
+	public boolean compareValue(Card card) {
+		
+		if(this.value < card.getValue())
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	
+	public boolean compareRed(Card card)
+	{
+		if(this.red != card.getRed())
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	
+	public boolean compareColor(Card card)
+	{
+		if(this.color == card.getColor())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	
+}
---
+++ Solitaire_java/src/Pile.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Pile {
+	
+	public ArrayList<Card> cards;
+	
+	
+}
